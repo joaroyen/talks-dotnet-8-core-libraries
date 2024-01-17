f5c50825b819ae26c43233a89b2838e93681be9e
Joar Øyen <joaroyen@users.noreply.github.com>
2024-01-17 16:43:22 +0100

    Presentasjon i NNUG 18. januar 2024 (#1)
    * Empty presentation
    
    * Beta 1
    
    * Cosmetic fixes
    
    * Completed presentation
    
    * Spelling

---
+++ src/Dotnet8CoreLibraries/MetricsTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Dotnet8CoreLibraries;
+
+public class MetricsTests
+{
+    [Fact]
+    public void Metrics_can_be_created_with_tags_and_cached_by_factory()
+    {
+        var serviceProvider = new ServiceCollection().AddMetrics().BuildServiceProvider();
+        var meterFactory = serviceProvider.GetRequiredService<IMeterFactory>();
+
+        var meter = meterFactory.Create("JobExecutions");
+
+        var jobOneCounter = meter.CreateCounter<int>(
+            "JobOne", 
+            "execution", 
+            "Measure throughput of job executions", 
+            new Dictionary<string, object?>
+            {
+                ["location"] = "Trondheim"
+            });
+        
+        jobOneCounter.Add(1);
+    }
+}
---
+++ src/Dotnet8CoreLibraries/SerializationTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace Dotnet8CoreLibraries;
+
+public class SerializationTests
+{
+
+    [Fact]
+    public void Missing_members_can_be_required()
+    {
+        JsonSerializer.Deserialize<Person>(
+            """
+            {
+                "Firstname": "John",
+                "Lastname": "Doe",
+                "Age": 42
+            }
+            """);
+    }
+}
+
+[JsonUnmappedMemberHandling(JsonUnmappedMemberHandling.Disallow)]
+record Person(string Firstname, string Lastname);
